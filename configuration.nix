f29f6b957f10bb2d39379874a1ab7887231fd188
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-05-07 12:09:25 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,8 @@
-# Edit this configuration file to define what should be installed on
+hTa# Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, fetchTarball, ... }:
 
 let
 # add nixos-hardware channel declaratively
